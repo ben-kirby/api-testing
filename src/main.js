f5af3d79bab60410574fb0e28fb852600371a91f
Student <example@example.com>
2019-01-15 11:10:23 -0800

    Has two APIs, one for weather, one is a giphy search. jQuery versions are commented out, but working. Vanilla versions are implemented.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,93 @@
 import { Giphy } from './giphy';
+import $ from 'jquery';
 
 $(document).ready(function(){
+
+  // jQuery Weather App
+  // $('#weatherLocation').click(function() {
+  //   let city = $('#location').val();
+  //   $('#location').val("");
+  //   $.ajax({
+  //     url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER}`,
+  //     type: 'GET',
+  //     data: {
+  //       format: 'json'
+  //     },
+  //     success: function(response) {
+  //       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+  //       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+  //     },
+  //     error: function() {
+  //       $('#errors').text("There was an error processing your request. Please try again.");
+  //     }
+  //   });
+  // });
+
+
+// jQuery giphy search
+  // $('#search').click(function(){
+  //   let term = $('#searchterm').val();
+  //   $('#searchterm').val("");
+  //   $.ajax({
+  //     url: `http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_KEY}&q=${term}`,
+  //     type: 'GET',
+  //     data: {
+  //       format: 'json'
+  //     },
+  //     success: function(response){
+  //       $('.showURL').text(`${response.data[0].url}`)
+  //     },
+  //     error: function(){
+  //       $('#errors').text("There was an error.")
+  //     }
+  //   });
+  // });
+
+  //vanilla JS weather
+  $('#weatherLocation').click(function(){
+    const city = $('#location').val();
+    $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+        console.log("Hi");
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    const getElements = function(response){
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
+  });
+
+  //vanilla JS Giphy
   $('#search').click(function(){
-    let term = $('searchterm').val();
+    const term = $('#searchterm').val();
     $('#searchterm').val("");
-    $.ajax({
-      url: `api.giphy.com/v1/gifs/search?api_key=${}&q=pizza`
-    })
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_KEY}&q=${term}`;
+
+    request.onreadystatechange = function(){
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    const getElements = function(response){
+      $('.showURL').text(`${response.data[0].url}`);
+    }
   });
 });
